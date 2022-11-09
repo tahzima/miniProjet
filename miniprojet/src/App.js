e159238577630cae3d5c9bfd61540030d4f6e9f0
tahzima <tahzima.i@gmail.com>
2022-11-09 17:28:09 +0100

    some modification

--- miniprojet/src/App.js
+++ miniprojet/src/App.js
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 import './App.css';
-import navBar from './composents/navBar/navBar'
 
 function App() {
     return (
-      // <navBar/>
       <header>
         <div class="header ">
           <div class="logo">
@@ -24,15 +21,6 @@ function App() {
       </header>
       
     );
-=======
-
-function App() {
-  return (
-    <>
-        <h1 className="text-red-400" >test</h1>
-    </>
-  );
->>>>>>> 198c1e840ed714a18c5e3003121364f98a0f7e6e
 }
 
 export default App;
